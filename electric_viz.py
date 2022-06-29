642ec2f7114d2f7292145d1353c4c487af2be9bf
сmartem <c.martem@gmail.com>
2022-06-29 18:17:52 +0200

    Plots

---
+++ electric_viz.py
@@ -0,0 +1,7 @@
+import plotly.express as px
+import pandas as pd
+from asyncio.windows_utils import pipe
+dfb = pd.read_csv("C:\Users\3615\Documents\GitHub\data_viz\electric_data.csv")
+
+dfb.columns
+df = px.data.tips()
